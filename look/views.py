7dc0f14b9fc6c3ebf5e9821fb1aaf4c4ab66d615
Mrklata <lukasz98kowalczyk@gmail.com>
2020-04-07 22:26:28 +0200

    app version 2

--- look/views.py
+++ look/views.py
@@ -3,42 +3,42 @@ from look.models import Image, Movie
 
 
 def main_site(request):
-    return render(request, 'look/main_site.html')
+    return render(request, 'look/main_2.html')
 
 
 def bathroom(request):
     file = Image.objects.filter(section='bathroom')
-    return render(request, 'look/bathroom.html', {'file': file})
+    return render(request, 'look/bathroom_2.html', {'file': file})
 
 
 def kitchen(request):
     file = Image.objects.filter(section='kitchen')
-    return render(request, 'look/kitchen.html', {'file': file})
+    return render(request, 'look/kitchen2.html', {'file': file})
 
 
 def closet(request):
     file = Image.objects.filter(section='closet')
-    return render(request, "look/closet.html", {'file': file})
+    return render(request, "look/closet2.html", {'file': file})
 
 
 def beds(request):
     file = Image.objects.filter(section='beds')
-    return render(request, 'look/beds.html', {'file': file})
+    return render(request, 'look/beds2.html', {'file': file})
 
 
 def other(request):
     file = Image.objects.filter(section='other')
-    return render(request, 'look/other.html', {'file': file})
+    return render(request, 'look/others2.html', {'file': file})
 
 
 def movies(request):
     file = Movie.objects.all()
-    return render(request, 'look/movies.html', {'file': file})
+    return render(request, 'look/movies2.html', {'file': file})
 
 
 def from_back(request):
     file = Image.objects.filter(section='back')
-    return render(request, 'look/from_back.html', {'file': file})
+    return render(request, 'look/from_back2.html', {'file': file})
 
 
 def main_try(request):
